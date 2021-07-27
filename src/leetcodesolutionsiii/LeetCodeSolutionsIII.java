48aa595fed01a8be98772d331ca4404e5ee32bef
Phoenix <adit.parida1@gmail.com>
2021-07-27 12:14:30 -0700

    iml

--- src/leetcodesolutionsiii/LeetCodeSolutionsIII.java
+++ src/leetcodesolutionsiii/LeetCodeSolutionsIII.java
@@ -5,12 +5,22 @@ public class LeetCodeSolutionsIII
 {
     public static void main(String[] args)
     {
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(4);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(4);
-        root.right = new TreeNode(5);
-        root.right.right = new TreeNode(5);
-        System.out.println(new LongestUnivaluePath687().longestUnivaluePath(root));
+        int[] arr = {14,10,5,1,0};
+        //System.out.println(search(6, arr));
+//        System.out.println(new LongestUnivaluePath687().longestUnivaluePath(root));
+        System.out.println(solution(arr));
+    }
+    public static int solution(int[] A) 
+    {
+
+    Arrays.sort(A);
+    if(A.length <= 4)
+      return 0;
+    int min = Integer.MAX_VALUE;
+    for(int i =0; i < 4; ++i)
+    {
+       min = Math.min(min, A[A.length - 3 + i -1] - A[i]);
+    }
+    return min;
     }
 }
